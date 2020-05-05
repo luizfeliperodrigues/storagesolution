10cdc53d46c95ace46b37d80fbbf588b85902338
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-05-05 17:24:17 -0300

    instalando jquery mais atualizado

--- MarthaPadilhaLeiloes/frontend/src/app/app-routing.module.ts
+++ MarthaPadilhaLeiloes/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuctionComponent } from './auction/auction.component';
+import { ComitenteComponent } from './comitente/comitente.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auction', component: AuctionComponent},
+  {path: 'comitente', component: ComitenteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
